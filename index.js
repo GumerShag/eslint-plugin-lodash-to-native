4e7fdb6bc8511f3d1f0dd169c4571314dc7097a3
Gumer Shagiyanov <>
2019-09-26 01:41:12 +0300

    * Add configs * Add rules

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ module.exports = {
     configs: {
         recommended: {
             plugins: ['lodash-to-native'],
+            env: {
+                browser: true,
+                es6: true,
+                node: true
+            },
             rules: {
                 'lodash-to-native/map': 'warn'
             }
         }
     },
     rules: {
-        "map": require('./lib/map')
+        "map": require('./lib/map/map.js')
     }
 };
--- tests/lib/rules/rule-test.js
+++ tests/lib/rules/rule-test.js
@@ -2,4 +2,16 @@ const mapRule = require('../../../map/map.js');
 const { RuleTester } = require('eslint');
 
 let ruleTester = new RuleTester();
-ruleTester.run("map-rule", {});
+ruleTester.run("map", mapRule, {
+    valid: [],
+    invalid: [
+        {
+            code: "_.map([], function() {})",
+            errors: [
+                {
+                    messageId: "Native Array.prototype.map should be used with arrays"
+                }
+            ]
+        }
+    ]
+});
